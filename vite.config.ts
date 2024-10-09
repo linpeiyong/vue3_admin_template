c4aeea7a27535a26eec9c6721fc9e2b51aff0942
linpeiyong <1415387989@qq.com>
2024-10-09 11:08:49 +0800

    配置文件同步

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,23 @@
-// vite.config.ts
-
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
-import { defineConfig } from 'vite';
+// https://vitejs.dev/config/
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { viteMockServe } from 'vite-plugin-mock';
-import { UserConfigExport, ConfigEnv } from 'vite';
 import path from 'path';
-export default defineConfig(({ command }) => {
+//引入svg需要用到插件
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+//mock插件提供方法
+import { viteMockServe } from 'vite-plugin-mock';
+export default defineConfig(({ command, mode }) => {
+    //获取各种环境下的对应的变量
+    let env = loadEnv(mode, process.cwd());
     return {
         plugins: [
             vue(),
             createSvgIconsPlugin({
-                // Specify the icon folder to be cached
                 iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-                // Specify symbolId format
                 symbolId: 'icon-[dir]-[name]',
             }),
-
             viteMockServe({
-                localEnabled: command === 'serve',
+                localEnabled: command === 'serve', //保证开发阶段可以使用mock接口
             }),
         ],
         resolve: {
@@ -26,6 +25,7 @@ export default defineConfig(({ command }) => {
                 '@': path.resolve('./src'), // 相对路径别名配置，使用 @ 代替 src
             },
         },
+        //scss全局变量一个配置
         css: {
             preprocessorOptions: {
                 scss: {
