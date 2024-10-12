21a89737ac07f5dcd1c7c328146466d2623a22b0
linpeiyong <1415387989@qq.com>
2024-10-12 09:59:01 +0800

    路由拦截器,请求拦截器

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -14,10 +14,6 @@ import { onMounted } from 'vue';
 import useUserStore from '@/store/modules/user';
 
 let userStore = useUserStore();
-
-onMounted(() => {
-    userStore.userInfo();
-});
 </script>
 
 <style scoped></style>
