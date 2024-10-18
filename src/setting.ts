266f10ba5e268f122690928f7205e0d155c859e7
linpeiyong <1415387989@qq.com>
2024-10-18 15:20:06 +0800

    属性模块

--- src/setting.ts
+++ src/setting.ts
@@ -1,7 +1,6 @@
+//用于项目logo|标题配置
 export default {
-    title: '硅谷真选平台',
-
-    logo: '/public/logo.png',
-
-    logoHidden: true,
+    title: '硅谷甄选运营平台', //项目的标题
+    logo: '/logo.png', //项目logo设置
+    logoHidden: true, //logo组件是否隐藏设置
 };
--- src/store/modules/type/type.ts
+++ src/store/modules/type/type.ts
@@ -1,5 +1,5 @@
 import type { RouteRecordRaw } from 'vue-router';
-// import type { CategoryObj } from '@/api/product/attr/type';
+import type { CategoryObj } from '@/api/product/attr/type';
 //定义小仓库数据state类型
 export interface UserState {
     token: string | null;
@@ -10,11 +10,11 @@ export interface UserState {
 }
 
 // //定义分类仓库state对象的ts类型
-// export interface CategoryState {
-//     c1Id: string | number;
-//     c1Arr: CategoryObj[];
-//     c2Arr: CategoryObj[];
-//     c2Id: string | number;
-//     c3Arr: CategoryObj[];
-//     c3Id: string | number;
-// }
+export interface CategoryState {
+    c1Id: string | number;
+    c1Arr: CategoryObj[];
+    c2Arr: CategoryObj[];
+    c2Id: string | number;
+    c3Arr: CategoryObj[];
+    c3Id: string | number;
+}
