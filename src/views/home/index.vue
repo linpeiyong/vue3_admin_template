e5f2db0fe82b2e89e22b95fe6d2c4b8e26f3bd2c
linpeiyong <1415387989@qq.com>
2025-04-10 11:37:02 +0800

    主页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,25 +1,67 @@
 <!--
- * new page
- * @author: LinPeiYong
- * @since: 2024-10-10
- * index.vue
+ * @Description: Stay hungry，Stay foolish
+ * @Author: Huccct
+ * @Date: 2023-05-19 17:38:54
+ * @LastEditors: Huccct
+ * @LastEditTime: 2023-06-02 15:09:08
 -->
+<script setup lang="ts">
+import { getTime } from '@/utils/time';
+import useUserStore from '@/store/modules/user';
+let userStore = useUserStore();
+</script>
 <template>
-    <div>
-        {{ userStore.username }}
+    <el-card>
+        <div class="box">
+            <img :src="userStore.avatar" alt="" class="avatar" />
+            <div class="footer">
+                <h3 class="title">
+                    {{ getTime() }}好~
+                    <span class="gradient">{{ userStore.username }}</span>
+                </h3>
+                <p class="subtitle">Vue-Admin</p>
+            </div>
+        </div>
+    </el-card>
+    <div class="bottom">
+        <def-svg-icon name="welcome" width="600px" height="300px"></def-svg-icon>
     </div>
 </template>
-
-<script setup lang="ts">
-import { onMounted } from 'vue';
-
-import useUserStore from '@/store/modules/user';
-
-let userStore = useUserStore();
-
-onMounted(() => {
-    userStore.userInfo();
-});
-</script>
-
-<style scoped></style>
+<style lang="scss" scoped>
+.box {
+    display: flex;
+    .avatar {
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+    }
+    .footer {
+        margin-left: 20px;
+        margin-top: 15px;
+        .title {
+            font-size: 35px;
+            margin-bottom: 30px;
+            font-weight: 900;
+            .gradient {
+                background: linear-gradient(to right, #001529, #001529, #ffffff);
+                /* 渐变方向是从左到右，颜色从红色到绿色 */
+                background-clip: text;
+                -webkit-background-clip: text; /* 兼容WebKit浏览器（例如Chrome和Safari） */
+                color: transparent;
+                font-size: 24px;
+                font-weight: bold;
+            }
+        }
+        .subtitle {
+            font-style: italic;
+            color: #ccc;
+            font-weight: 700;
+        }
+    }
+}
+.bottom {
+    margin-top: 30px;
+    display: flex;
+    justify-content: center;
+}
+</style>
